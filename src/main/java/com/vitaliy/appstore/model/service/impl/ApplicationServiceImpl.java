e584180aeee5ddd16299ab74e2587be3c5343e7d
Vitalij Stinskij <vstinskij@gmail.com>
2021-10-01 12:32:06 +0300

    added application service implementation class

---
+++ src/main/java/com/vitaliy/appstore/model/service/impl/ApplicationServiceImpl.java
@@ -0,0 +1,19 @@
+package com.vitaliy.appstore.model.service.impl;
+
+
+import com.vitaliy.appstore.model.dao.ApplicationRepository;
+import com.vitaliy.appstore.model.service.ApplicationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ApplicationServiceImpl implements ApplicationService {
+
+    private ApplicationRepository applicationRepository;
+
+    @Autowired
+    public ApplicationServiceImpl(ApplicationRepository applicationRepository) {
+        this.applicationRepository = applicationRepository;
+    }
+
+}
