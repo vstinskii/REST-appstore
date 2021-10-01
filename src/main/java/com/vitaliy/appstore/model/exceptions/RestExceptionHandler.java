323f60c7eceaefa6575739d8e16fcf0c8452f490
Vitalij Stinskij <vstinskij@gmail.com>
2021-10-01 12:54:31 +0300

    added application not found exception method to create JSON exception message

--- src/main/java/com/vitaliy/appstore/model/exceptions/RestExceptionHandler.java
+++ src/main/java/com/vitaliy/appstore/model/exceptions/RestExceptionHandler.java
@@ -1,9 +1,24 @@
 package com.vitaliy.appstore.model.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class RestExceptionHandler {
+
+    @ExceptionHandler
+    public ResponseEntity<ApplicationStackTrace> handleApplicationNotFoundException(ApplicationNotFoundException e) {
+
+        ApplicationStackTrace stackTrace = new ApplicationStackTrace();
+
+        stackTrace.setStatus(HttpStatus.NOT_FOUND.value());
+        stackTrace.setMessage(e.getMessage());
+        stackTrace.setTimeStamp(System.currentTimeMillis());
+
+        return new ResponseEntity<>(stackTrace, HttpStatus.NOT_FOUND);
+    }
 
 
 }
