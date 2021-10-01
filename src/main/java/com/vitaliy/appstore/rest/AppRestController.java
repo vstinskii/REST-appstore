db6ff10a7ab15eda78e356b2eec12cf74c44781b
Vitalij Stinskij <vstinskij@gmail.com>
2021-10-01 15:11:34 +0300

    removed excess import in rest controller

--- src/main/java/com/vitaliy/appstore/rest/AppRestController.java
+++ src/main/java/com/vitaliy/appstore/rest/AppRestController.java
@@ -3,7 +3,6 @@ package com.vitaliy.appstore.rest;
 import com.vitaliy.appstore.model.entity.Application;
 import com.vitaliy.appstore.model.service.ApplicationService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
