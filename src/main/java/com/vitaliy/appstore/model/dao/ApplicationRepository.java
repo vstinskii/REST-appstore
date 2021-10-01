b4a75c70a18d414f4a918a93900cfee437ee64e5
Vitalij Stinskij <vstinskij@gmail.com>
2021-10-01 13:24:50 +0300

    added getCountOfApplicationByContentRate using SQL query method to ApplicationRepository

--- src/main/java/com/vitaliy/appstore/model/dao/ApplicationRepository.java
+++ src/main/java/com/vitaliy/appstore/model/dao/ApplicationRepository.java
@@ -2,7 +2,12 @@ package com.vitaliy.appstore.model.dao;
 
 import com.vitaliy.appstore.model.entity.Application;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ApplicationRepository extends JpaRepository<Application, Integer> {
 
+    @Query(value = "SELECT COUNT(*) FROM application WHERE content_rate=:contentRate", nativeQuery = true)
+    int getCountOfApplicationByContentRate(@Param("contentRate") int contentRate);
+    
 }
